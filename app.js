a6dbfb335c8ee5c8337911b6abc52ab0644192cd
Fozil <fozilbeksadullaev1@gmail.com>
2025-01-19 00:39:24 +0100

    adding some javascript codes to the app.js and changing and creating ejs files to the views folder

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const express = require('express');
 // express app
 const app = express();
 
-// liste for requests
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>home page</p>');
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.send('<p>about page</p>');
+    res.render('about');
+});
+
+// redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404 page
+
+app.use((req, res) => {
+    res.status(404).render('404');
 });

01c056326dbd6d65196427db8e21a6219b7301b1
Fozil <fozilbeksadullaev1@gmail.com>
2025-01-19 20:25:37 +0100

    added data to mongodb using mongoose

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const Blog = require('./models/blog');
 // express app
 const app = express();
 // MongoDB connection string
 const dbURI = 'mongodb+srv://JGPau2suaN2CFKgE:fozil2002f@node-tuts.kdrvs.mongodb.net/?retryWrites=true&w=majority&appName=node-tuts';
 
-mongoose.connect(dbURI);
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-}).on('error', (error) => {
-    console.log('Error:', error);
-});
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then((result) => app.listen(3000))
+   .catch((error) => console.log(error));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
-
 // Middleware & static files
 app.use(express.static('public')); // Serve static files
-
-
 app.use(morgan('dev'));
+
 
 // Home route
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yagr finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Fozil finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat Yagr', snippet: 'Lorem ipsum dolor sit amet consectetur' }
-    ];
-    res.render('index', { title: 'Home', blogs: blogs}); // Pass `title` and `blogs` to the view
+    res.redirect('/blogs');
+
 });
 
 // About route
@@ -41,6 +31,16 @@ app.get('/about', (req, res) => {
 });
 
 // Create a new blog route
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result }); 
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 });

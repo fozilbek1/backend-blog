c981069f5775a8d701351335cc36f51126f99a81
Fozil <fozilbeksadullaev1@gmail.com>
2025-01-19 01:16:04 +0100

    Created a new create.ejs file

--- app.js
+++ app.js
@@ -17,10 +17,9 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 
 // 404 page
 

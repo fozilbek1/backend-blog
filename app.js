3c4104dbd34358ab769e037e9d477e0b587280e1
Fozil <fozilbeksadullaev1@gmail.com>
2025-01-19 03:02:29 +0100

    fixed some errors in ejs files

--- app.js
+++ app.js
@@ -7,22 +7,31 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
 
+// Home route
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home'});
+    const blogs = [
+        { title: 'Yagr finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Fozil finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat Yagr', snippet: 'Lorem ipsum dolor sit amet consectetur' }
+    ];
+    res.render('index', { title: 'Home', blogs }); // Pass `title` and `blogs` to the view
 });
 
+// About route
 app.get('/about', (req, res) => {
-    res.render('about', {title: 'About'});
+    res.render('about', { title: 'About' });
 });
 
+// Create a new blog route
 app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
+    res.render('create', { title: 'Create a new blog' });
+});
 
 // 404 page
-
 app.use((req, res) => {
-    res.status(404).render('404', {title: '404'});
+    res.status(404).render('404', { title: '404' });
 });

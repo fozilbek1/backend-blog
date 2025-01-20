bbb03dd4c21bf0289770401155c8083fc930d20f
Fozil <fozilbeksadullaev1@gmail.com>
2025-01-20 13:43:21 +0100

    handling json errors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // MongoDB connection string
 const dbURI = 'mongodb+srv://JGPau2suaN2CFKgE:fozil2002f@node-tuts.kdrvs.mongodb.net/?retryWrites=true&w=majority&appName=node-tuts';
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI)
    .then((result) => app.listen(3000))
    .catch((error) => console.log(error));
 
@@ -16,7 +16,9 @@ app.set('view engine', 'ejs');
 
 // Middleware & static files
 app.use(express.static('public')); // Serve static files
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies    
 app.use(morgan('dev'));
+
 
 
 // Home route
@@ -41,9 +43,49 @@ app.get('/blogs', (req, res) => {
     });
 })
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+})
+
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 });
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then((result) => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+});
+
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({ redirect: '/blogs' });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+ 
+})
+
+
 
 // 404 page
 app.use((req, res) => {
